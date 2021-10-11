1007167dfb861e1b70f74b37635dc3725b8f2554
Hiroki Yorimitsu <yorimitsu@supinf.co.jp>
2021-10-11 12:59:53 +0900

    update: change to an application that sends HTTP Request

--- app-api/main.go
+++ app-api/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
-func handler(str string) func(http.ResponseWriter, *http.Request) {
-	return func(writer http.ResponseWriter, request *http.Request) {
-		fmt.Fprintf(writer, str)
+func send(url string) string {
+	req, _ := http.NewRequest("GET", url, nil)
+
+	client := new(http.Client)
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	byteArray, _ := ioutil.ReadAll(resp.Body)
+	return string(byteArray)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	url, ok := r.URL.Query()["url"]
+	if !ok || len(url[0]) < 1 {
+		fmt.Fprintf(w, "Hello, eBPF!")
+		return
 	}
+
+	resp := send(url[0])
+	fmt.Fprintf(w, resp)
 }
 
 func main() {
-	http.HandleFunc("/", handler("Hello, World!"))
+	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
